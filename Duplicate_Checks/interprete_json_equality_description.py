45cd59d98a25b1b0359c799907bbcf0f5f94ddd2
DESKTOP-8VEJUR0\CEOS <nchrisr@myumanitoba.ca>
2019-06-07 11:05:14 -0500

    Added documentation to interprete_json_equality_description.py and updated the documentation.

--- Duplicate_Checks/interprete_json_equality_description.py
+++ Duplicate_Checks/interprete_json_equality_description.py
@@ -1,6 +1,9 @@
+"""This script will read the json files which describe the duplicates of the raw files, and print the details."""
+
 import json
 import pathlib2
 
+# Constants for the names of json files and the types of their content.
 CONTENT = "CONTENT"
 NAME = "NAME"
 NAME_AND_CONTENTS = "NAME AND CONTENTS"
@@ -11,6 +14,7 @@ EQUALITY_OF_NAME_AND_CONTENTS = "equality_by_name_and_contents.json"
 
 
 def read_jsons(json_content, type):
+    """process the json_content based on the type argument. Print the details. """
     if type == CONTENT:
         for item in json_content:
             directory_1 = pathlib2.Path(json_content[item][0])
@@ -36,6 +40,7 @@ def read_jsons(json_content, type):
 
 
 def main():
+    """Read in json files and print the details."""
     with open(EQUALITY_OF_CONTENTS, 'r') as json_contents:
         equality_of_contents_data = json.load(json_contents)
     read_jsons(equality_of_contents_data, CONTENT)
@@ -48,9 +53,10 @@ def main():
         equality_of_name_and_contents_data = json.load(json_contents)
     read_jsons(equality_of_name_and_contents_data, NAME_AND_CONTENTS)
 
-
-    print("\nNumber of files with same contents: {}\nNumber of files with same names: {}\n"
-          "Number of files with same name and contents: {}."
+    # Print the number of file found to have duplicates for each kind of duplicates.
+    print("\nNumber of files with same contents: {}"
+          "\nNumber of files with same names: {}"
+          "\nNumber of files with same name and contents: {}."
           .format(len(equality_of_contents_data), len(equality_of_name_data),
                   len(equality_of_name_and_contents_data)))
 
