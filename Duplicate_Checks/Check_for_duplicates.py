cb1e46c5381a950ae334bbc9820e29109af814df
DESKTOP-8VEJUR0\CEOS <nchrisr@myumanitoba.ca>
2019-06-07 11:01:03 -0500

    Added documentation to check_for_duplicates.py and updated the documentation.

--- Duplicate_Checks/Check_for_duplicates.py
+++ Duplicate_Checks/Check_for_duplicates.py
@@ -1,6 +1,7 @@
-"""This script will check spceified folders and compare their contents to look for files with the same name """
-import pandas as pd
-from pandas.testing import assert_frame_equal
+"""
+This script will check specified folders and compare their contents to look for files with the same name and contents.
+The folders must be specified as command line arguments.
+"""
 import os
 import pathlib2
 import json
@@ -9,71 +10,57 @@ import sys
 
 ARGS_COUNT = 3
 arguments = sys.argv
-if len(arguments)< ARGS_COUNT:
+if len(arguments) < ARGS_COUNT:
     print("Incomplete arguments... {} arguments received, {} required ".format(len(arguments), ARGS_COUNT))
 
-FIRST_DIRECTORY = arguments[1] #"C:\Users\CEOS\Desktop\IMB DATA_CHECKS\ARCTICNET 2009 SEAICE\IMB"
-SECOND_DIRECTORY = arguments[2] #"C:\Users\CEOS\Desktop\IMB DATA_CHECKS\IMB DATA BACKUP\IMB_Data_Backup"
+FIRST_DIRECTORY = arguments[1]
+SECOND_DIRECTORY = arguments[2]
 
-
+# Dictionaries to hold the information about duplicated files.
+# One for duplicates based on name only, one for duplicates based on contents only, one for duplicates based on name
+# and content.
 equality_by_name_dict = {}
 equality_by_contents_dict = {}
-equality_by_name_and_content_dict = {}
-
-
-def read_data(path):
-    """Read in the file specified by path based on the extension of the file."""
-    if path.lower().endswith('.log'):
-        d_frame = pd.read_csv(path)
-    else:
-        print("Invalid file type provided {}".format(path.lower()))
-        exit(-1)
-    return d_frame
-
-
-def check_dataframe_equality(df1, df2):
-    """Check that the two data frames df1 and df2 are equal """
-    try:
-        return_val = assert_frame_equal(df1, df2, check_dtype=False)
-    except Exception as e:
-        print ("The Dataframes are different...")
-        print(e)
-        return_val = False
-    if return_val is None:
-        return_val = True
-    return return_val
+equality_by_name_and_contents_dict = {}
 
 
 def check_for_same_files(directory_1, directory_2):
+    """This function will check the directories specified as the arguments,
+    for files which have same name or same contents and both (have same name and contents)."""
     directory_1_files = os.listdir(directory_1)
     directory_2_files = os.listdir(directory_2)
 
+    # Print details of processing.
     print("{} files in {}; {} files in {}".format(len(directory_1_files), directory_1,
                                                   len(directory_2_files), directory_2))
 
+    # For every file in the first specified arguments.
     for item_1 in directory_1_files:
+        # Only process '.log' files.
         if (item_1.lower()).endswith('.log'):
             print("\nChecking for duplicates for {}".format(item_1))
             for item_2 in directory_2_files:
                 if (item_1.lower()).endswith('.log'):
-                    # df_item_1 = read_data(str(pathlib2.Path(directory_1, item_1)))
-                    # df_item_2 = read_data(str(pathlib2.Path(directory_2, item_2)))
+                    # Check for name equality
                     name_equality = item_1.lower() == item_2.lower()
+                    # Check for content equality
                     content_equality = filecmp.cmp(str(pathlib2.Path(directory_1, item_1)),
                                                    str(pathlib2.Path(directory_2, item_2)))
-                    if name_equality and not(content_equality):
-                        print("JH")
+                    if name_equality and (not(content_equality)):
+                        print("{} has a duplicate name but not same contents.".format(item_1))
+
+                    # Update the dictionaries representing the findings of the script.
                     if name_equality:
                         equality_by_name_dict[item_1] = [directory_1, directory_2]
                     if content_equality:
                         equality_by_contents_dict[item_1] = [directory_1, directory_2]
                     if name_equality and content_equality:
-                        equality_by_name_and_content_dict[item_1] = [directory_1, directory_2]
-
+                        equality_by_name_and_contents_dict[item_1] = [directory_1, directory_2]
     return
 
 
 def main():
+    """This function calls check_for_same_files and writes the results to their respective json files. """
     check_for_same_files(FIRST_DIRECTORY, SECOND_DIRECTORY)
 
     with open('equality_by_name.json', 'w') as out_f:
@@ -83,7 +70,7 @@ def main():
         json.dump(equality_by_contents_dict, n_out_f, indent=4)
 
     with open('equality_by_name_and_contents.json', 'w') as n_out_ff:
-        json.dump(equality_by_name_and_content_dict, n_out_ff, indent=4)
+        json.dump(equality_by_name_and_contents_dict, n_out_ff, indent=4)
 
     return
 
