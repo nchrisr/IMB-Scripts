15209ce9e5288d8699e0001c3454b27a3fa8bc75
DESKTOP-8VEJUR0\CEOS <nchrisr@myumanitoba.ca>
2019-06-10 16:28:41 -0500

    -> File processor.py was completed but fails on a number of files -> Using a new strategy -> Added standard_processing.py which breaks up a log file into multiple data files.

---
+++ File_Processing/standard_processing.py
@@ -0,0 +1,69 @@
+import sys
+import os
+import pathlib2
+import shutil
+
+WORKING_DIRECTORY = None
+if len(sys.argv) < 2:
+    print("Incomplete Arguments... at lease one argument required")
+    exit(-1)
+else:
+    WORKING_DIRECTORY = sys.argv[1]
+
+NO_CARRIER = "NO CARRIER"
+CONNECT = "CONNECT"
+RING = "RING"
+DASHES = "-----------"
+OUTPUTS_FOLDER = "Outputs"
+
+
+def process_regular_imb_files(working_directory=WORKING_DIRECTORY):
+    """This function will process the IMB log files in the directory 'working_directory' and extract the data from it.
+       For each time a connection was successfully and data was successfully transmitted,
+       the data will be put into a file with the naming convention:
+         'original_file'-'file_count'.log
+       This file will be saved into a directory named after the original file.
+    """
+    # Get the list of files in the directory.
+    files_to_process = os.listdir(working_directory)
+    print("{} files will be processed.".format(len(files_to_process)))
+    # Make an output directory within the working directory
+    outputs_path = str(pathlib2.Path(working_directory, OUTPUTS_FOLDER))
+    os.mkdir(outputs_path)
+
+    for file_ in files_to_process:
+        if file_.endswith(".log"):
+            try:
+                # Get the full file path for the file being processed, and get the name of the file without the extension.
+                file_n = str(pathlib2.Path(working_directory, file_))
+                file_name_use = file_.split(".")[0]
+
+                # Make a directory named after the file being processed.
+                move_path = str(pathlib2.Path(outputs_path, file_name_use))
+                os.mkdir(move_path)
+
+                # Open the file and start reading, for each dataset extracted, put it in the variable, 'csv_data'
+                f = open(file_n, 'rb')
+                csv_data = f.read()
+
+                parse_list = csv_data.split(NO_CARRIER)
+
+                # Add the number of rings to the data for the file
+                # Create the file and write the data into it.
+                # Move the file into a directory named after the file which was processed to generate it.
+                for i in range(0, len(parse_list)):
+                    created_file_name =file_name_use + "-" + str(i+1) + ".log"
+                    curr_f = open(created_file_name, "w")
+                    curr_f.write(parse_list[i])
+                    curr_f.close()
+                    shutil.move(created_file_name, move_path)
+                print("Successfully processed {}".format(file_))
+
+            except Exception as e:
+                print(e)
+                print "Processing {} failed".format(file_)
+
+    return
+
+
+process_regular_imb_files()
