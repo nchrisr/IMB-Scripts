38abf4c1e226a6c46891e00ee5792aa1e568da2b
DESKTOP-8VEJUR0\CEOS <nchrisr@myumanitoba.ca>
2019-07-18 14:58:30 -0500

    -> Fixed bugs in the clean_data function -> Added functionality to get the metadata when loading the data into the database.

--- File_Processing/load_db.py
+++ File_Processing/load_db.py
@@ -6,6 +6,7 @@ import sys
 import pathlib2
 import os
 import re
+import datetime
 import numpy as np
 
 # This dictionary is a representation of the directory structure which should be generated before running this script
@@ -32,7 +33,48 @@ THERMISTOR_TEMPERATURE_HEADERS = "Thermistor Temperature-"
 
 
 def clean_data(data_file):
+    original_file = str((pathlib2.Path(data_file).parent).name)
+    cleaned_file = str(pathlib2.Path(data_file).name)
     dframe = pd.read_csv(data_file, skiprows=6, dtype=str)
+    metadata = pd.read_csv(data_file, nrows=4, header=None)
+    metadata_dictionary = {}
+    for index, current_row in metadata.iterrows():
+        key = ((str(current_row[0])).lower()).replace(' ', '_')
+        value = str(current_row[1])
+
+        if key == "rings":
+            value = int(value)
+        elif key == "connection_string":
+            if not(bool(value.strip())):
+                value = None
+            else:
+                if value.strip() == "None":
+                    value = None
+        elif key == "imb_id":
+            if not(bool(value.strip())):
+                value = None
+            else:
+                if value.strip() == "None":
+                    value = None
+        elif key == "transmission_finished_successfully":
+            if value.strip() == "True":
+                value = True
+            else:
+                value = False
+        elif key == "data_line_after_connection_string":
+            if not(bool(value.strip())):
+                value = None
+            else:
+                if value.strip() == "None":
+                    value = None
+
+        metadata_dictionary[key] = value
+        print("H")
+    metadata_dictionary["original_file_name"] = original_file
+    metadata_dictionary["processed_file"] = cleaned_file
+    print(metadata_dictionary)
+
+
     #dframe.replace("nan", np.nan, inplace=True)
     #dframe.replace(to_replace=[None], value=np.nan, inplace=True)
     csv_str = dframe.to_csv()
@@ -45,6 +87,14 @@ def clean_data(data_file):
         for key in row:
             if row[key] in BAD_SYMBOLS:
                 row[key] = None
+            if key == "Device_Datetime_UTC":
+                try:
+                    row[key] = datetime.datetime.strptime(row[key], "%Y-%m-%d %H:%M:%S")
+                except Exception as e:
+                    try:
+                        row[key] = datetime.datetime.strptime(row[key], '%Y-%m-%d %H:%M:%S"')
+                    except Exception as e:
+                        row[key] = None
             if key == "$GPGGA":
                 try:
                     if row[key] != "$GPGGA":
@@ -100,7 +150,7 @@ def clean_data(data_file):
                     row[key] = float(row[key])
                 except Exception as e:
                     row[key] = None
-            elif key == "altitude units (M(metres)/F(feet))":
+            elif key == "altitude units (M(metres)/ F(feet))":
                 try:
                     if row[key].lower() != "m" and row[key].lower() != "f":
                         row["key"] = None
@@ -111,7 +161,7 @@ def clean_data(data_file):
                     row[key] = float(row[key])
                 except Exception as e:
                     row[key] = None
-            elif key == "Geoidal Separation Units (M(metres)/F(feet))":
+            elif key == "Geoidal Separation Units (M(metres)/ F(feet))":
                 try:
                     if row[key].lower() != "m" and row[key].lower() != "f":
                         row["key"] = None
@@ -130,6 +180,7 @@ def clean_data(data_file):
                     row[key] = None
             elif key == "Month":
                 try:
+                    row[key] = int(row[key])
                     if not(1 <= int(row[key]) <= 12):
                         row[key] = None
                 except Exception as e:
@@ -137,18 +188,21 @@ def clean_data(data_file):
 
             elif key == "Day":
                 try:
+                    row[key] = int(row[key])
                     if not (1 <= int(row[key]) <= 31):
                         row[key] = None
                 except Exception as e:
                     row[key] = None
             elif key == "Day of Year":
                 try:
-                    if not(1 <= int(row[key]) <= 365):
+                    row[key] = int(row[key])
+                    if not(1 <= row[key] <= 365):
                         row[key] = None
                 except Exception as e:
                     row[key] = None
             elif key == "Hour":
                 try:
+                    row[key] = int(row[key])
                     if not(0 <= int(row[key]) <= 23):
                         row[key] = None
                 except Exception as e:
@@ -159,7 +213,7 @@ def clean_data(data_file):
                         row[key] = None
                 except Exception as e:
                     row[key] = None
-            elif key == "Second":
+            elif key == "Seconds":
                 try:
                     row[key] = int(row[key])
                 except Exception as e:
@@ -175,7 +229,7 @@ def clean_data(data_file):
                         row[key] = None
                 except Exception as e:
                     row[key] = None
-            elif key == "UW sounder distance" or key == "Raw sounder quality" or key == "Corrected Snow sounder distance" or key == "Vattery Voltage":
+            elif key == "UW sounder distance" or key == "Raw snow sounder distance" or key == "Raw sounder quality" or key == "Corrected Snow sounder distance" or key == "Snow sounder quality":
                 try:
                     row[key] = float(row[key])
                 except Exception as e:
@@ -183,10 +237,45 @@ def clean_data(data_file):
 
             elif THERMISTOR_TEMPERATURE_HEADERS in key:
                 try:
-                    float(row[key])
+                    row[key] = float(row[key])
                 except Exception as e:
                     row[key] = None
+
+        gpgga = row["$GPGGA"]
+        del row["$GPGGA"]
+        row["GPGGA"] = gpgga
+
+        latitude = row["Latitude_degrees_decimal_minutes_ddmm.mmmm"]
+        del row["Latitude_degrees_decimal_minutes_ddmm.mmmm"]
+        row["Latitude_ddmm"] = latitude
+
+        n_s = row["N/S"]
+        del row["N/S"]
+        row["latitude_cardinal_point"] = n_s
+
+        longitude = row["Longitude_degrees_decimal_minutes_ddmm.mmmm"]
+        del row["Longitude_degrees_decimal_minutes_ddmm.mmmm"]
+        row["Longitude_ddmm"] = longitude
+
+        w_e = row["W/E"]
+        del row["W/E"]
+        row["longitude_cardinal_point"] = w_e
+
+        hdop = row["HDOP(horizontal dilution of precision)"]
+        del row["HDOP(horizontal dilution of precision)"]
+        row["HDOP"] = hdop
+
+        alt_units = row["altitude units (M(metres)/ F(feet))"]
+        del row["altitude units (M(metres)/ F(feet))"]
+        row["altitude_units"] = alt_units
+
+        geoidal_units = row["Geoidal Separation Units (M(metres)/ F(feet))"]
+        del row["Geoidal Separation Units (M(metres)/ F(feet))"]
+        row["Geoidal Separation Units"] = geoidal_units
+
+        row["metadata"] = metadata_dictionary
         data_list.append(row)
+
     return data_list
 
 
